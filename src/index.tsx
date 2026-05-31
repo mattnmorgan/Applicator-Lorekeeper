1f2431386f7edfa158588f29dc7c48de0c03d280
Matthew Morgan <daemicwolfie@gmail.com>
2026-05-31 19:14:07 -0400

    DEV-147

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,3 @@
 export { default as Lorekeeper } from "./apps/Lorekeeper";
 export { default as Lorebooks } from "./apps/Lorebooks";
+export { default as GuestLorebook } from "./apps/GuestLorebook";
